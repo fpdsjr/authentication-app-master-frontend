f8860337b343dc348fe8311f4d194df21b7ca11d
fpdsjr <flaviopds.jr@gmail.com>
2022-03-06 21:53:03 -0300

    feat: implement Home component

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,16 +1,29 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthProvider/useAuth';
 
 // import { Container } from './styles';
 
 const LoginInputs: React.FC = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const auth = useAuth();
+  const navigate = useNavigate();
+
+  async function handleSubmit() {
+    try {
+      await auth.authenticate(email, password);
+      navigate('/profile');
+    } catch (error) {
+      console.log('Invalid email or password');
+    }
+  }
 
   return (
     <>
       <input value={email} onChange={(e) => setEmail(e.target.value)} />
       <input value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button>Login</button>
+      <button onClick={handleSubmit}>Login</button>
     </>
   );
 };
