208e0f0beeec5e01a4ba20d0ce1c7477cdcfa5ae
fpdsjr <flaviopds.jr@gmail.com>
2022-03-06 21:51:51 -0300

    feat: fix api endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,19 @@
 import axios from 'axios';
+import { getUserLocalStorage } from '../context/AuthProvider/utils';
 
 export const Api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://localhost:4000',
 });
+
+Api.interceptors.request.use(
+  (config: any) => {
+    const user = getUserLocalStorage();
+
+    config.headers.Authorization = user?.token ? `Bearer ${user?.token}` : '';
+
+    return config;
+  },
+  (error) => {
+    return console.log('test');
+  }
+);
