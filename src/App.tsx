d7b31c2522d9e5fe45259cdd7f83cfca36f8cae6
fpdsjr <flaviopds.jr@gmail.com>
2022-03-06 21:52:27 -0300

    feat: implemente routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import LoginInputs from './components/LoginInputs';
+import LoginInputs from './components/Login';
 import { AuthProvider } from './context/AuthProvider';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ShowInfos } from './components/ShowInfos';
+import { Home } from './components/Home';
+import UserInfos from './components/UserInfos';
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/profile" element={<ShowInfos />}></Route>
-          <Route path="/login"></Route>
+          <Route
+            path="/profile"
+            element={
+              <ShowInfos>
+                <UserInfos />
+              </ShowInfos>
+            }></Route>
+          <Route path="/login" element={<LoginInputs />}></Route>
+          <Route path="/" element={<Home />}></Route>
         </Routes>
       </BrowserRouter>
     </AuthProvider>
