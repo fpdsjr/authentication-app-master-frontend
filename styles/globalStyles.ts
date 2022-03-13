ce0d4e197640cd14053c19d00753beba5a4a3ec2
fpdsjr <flaviopds.jr@gmail.com>
2022-03-13 02:59:14 -0300

    feat: add styledcompoenents globalstyle

---
+++ styles/globalStyles.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: teal;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+    background-color: black;
+    color: red;
+  }
+`;
+
+export default GlobalStyle;
